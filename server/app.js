a2d2d9914633f24fc67857d8f4f59b1efc8d19a5
Jenny Knuth <inkboco@gmail.com>
2015-10-31 10:11:03 -0600

    added facebook auth

--- server/app.js
+++ server/app.js
@@ -1,4 +1,7 @@
 // dependencies
+
+require('dotenv').load();
+
 var express = require('express'),
     logger = require('morgan'),
     cookieParser = require('cookie-parser'),
@@ -8,10 +11,15 @@ var express = require('express'),
     hash = require('bcrypt-nodejs'),
     path = require('path'),
     passport = require('passport'),
-    localStrategy = require('passport-local' ).Strategy;
+    config = require('./oauth.js'),
+    localStrategy = require('passport-local' ).Strategy,
+    FacebookStrategy = require('passport-facebook').Strategy,
+    TwitterStrategy = require('passport-twitter').Strategy,
+    GoogleStrategy = require('passport-google').Strategy;
+
 
 // mongoose
-mongoose.connect('mongodb://localhost/mean-auth');
+mongoose.connect(process.env.MONGOLAB_URI);
 
 // user schema/model
 var User = require('./models/user.js');
@@ -23,11 +31,14 @@ var app = express();
 var routes = require('./routes/api.js');
 
 // define middleware
+// app.set('views', __dirname + '/views');
+// app.set('view engine', 'jade');
 app.use(express.static(path.join(__dirname, '../client')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(express.methodOverride());
 app.use(require('express-session')({
     secret: 'keyboard cat',
     resave: false,
@@ -35,21 +46,69 @@ app.use(require('express-session')({
 }));
 app.use(passport.initialize());
 app.use(passport.session());
+app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
+
+// serialize and deserialize
+passport.serializeUser(function(user, done) {
+done(null, user);
+});
+passport.deserializeUser(function(obj, done) {
+done(null, obj);
+});
 
 // configure passport
 passport.use(new localStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
+passport.use(new FacebookStrategy({
+ clientID: config.facebook.clientID,
+ clientSecret: config.facebook.clientSecret,
+ callbackURL: config.facebook.callbackURL
+},
+function(accessToken, refreshToken, profile, done) {
+ process.nextTick(function () {
+   return done(null, profile);
+ });
+}
+));
+
 // routes
 app.use('/user/', routes);
 
-app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname, '../client', 'index.html'));
+app.get('/', routes.index);
+app.get('/ping', routes.ping);
+app.get('/', ensureAuthenticated, function(req, res){
+res.render('../client.partials.home', { user: req.user });
 });
 
-// error hndlers
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname, '../client', 'index.html'));
+  // res.render('login', { user: req.user });
+});
+
+app.get('/auth/facebook',
+passport.authenticate('facebook'),
+function(req, res){
+});
+app.get('/auth/facebook/callback',
+passport.authenticate('facebook', { failureRedirect: '/' }),
+function(req, res) {
+ res.redirect('/');
+});
+app.get('/logout', function(req, res){
+req.logout();
+res.redirect('/');
+});
+
+// test authentication
+function ensureAuthenticated(req, res, next) {
+if (req.isAuthenticated()) { return next(); }
+res.redirect('/')
+}
+
+// error handlers
 app.use(function(req, res, next) {
     var err = new Error('Not Found');
     err.status = 404;
