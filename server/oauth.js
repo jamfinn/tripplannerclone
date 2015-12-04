0a39ef1124dbfd25ae3d6033290ab66b8a9224e9
Jenny Knuth <inkboco@gmail.com>
2015-12-03 18:55:58 -0700

    added oauth.js back

---
+++ server/oauth.js
@@ -0,0 +1,18 @@
+var ids = {
+facebook: {
+ clientID: process.env.fbID,
+ clientSecret: process.env.fbSecret,
+ callbackURL: 'http://127.0.0.1:1337/auth/facebook/callback'
+},
+twitter: {
+ consumerKey: process.env.twitterID,
+ consumerSecret: process.env.twitterSecret,
+ callbackURL: "http://127.0.0.1:1337/auth/twitter/callback"
+},
+google: {
+ returnURL: '127.0.0.1:1337/auth/google/callback',
+ realm: 'http://127.0.0.1:1337'
+}
+}
+
+module.exports = ids
