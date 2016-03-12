dfa6d5ea48d4de0dc70ae6ee88184a62b4eacd1f
Jenny Knuth <inkboco@gmail.com>
2016-03-12 14:29:25 -0700

    get rid of JSON.parse

--- client/scripts/services.js
+++ client/scripts/services.js
@@ -12,7 +12,7 @@ app.factory('AuthService', ['$q', '$timeout', '$http', '$cookies', function ($q,
 
     authservice.getUserStatus = function() {
       // console.log('userid cookie item ', cookie.getItem('userid'));
-      console.log('user', JSON.parse($cookies.get('user')));
+      console.log('user', $cookies.get('user'));
       var id = $cookies.get('user');
 
 
