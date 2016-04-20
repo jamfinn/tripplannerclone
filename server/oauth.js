7b18bdec41237fd0244c8ab4edb6e6bf17e132a2
Jenny Knuth <inkboco@gmail.com>
2016-04-19 20:20:45 -0600

    update callbackURLs

--- server/oauth.js
+++ server/oauth.js
@@ -3,13 +3,13 @@ var ids = {
   facebook: {
    clientID: process.env.fbID,
    clientSecret: process.env.fbSecret,
-   callbackURL: 'https://triplannr.herokuapp.com/auth/facebook/callback'
+   callbackURL: 'http://demo.tripplanner.us/auth/facebook/callback'
   },
 
   google: {
     clientID: process.env.googleID,
     clientSecret: process.env.googleSecret,
-    callbackURL: 'https://triplannr.herokuapp.com/auth/google/callback'
+    callbackURL: 'http://demo.tripplanner.us/auth/google/callback'
   }
 
 }
